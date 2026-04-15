4bfc80c8862fbc5a60816bce1b840aaa3b950afd
pavlinkatt <126609209+pavlinkatt@users.noreply.github.com>
2026-04-15 22:49:26 +0200

    Initial project setup

---
+++ EarthquakeApp/src/main/java/mk/finki/ukim/mk/earthquakeapp/EarthquakeAppApplication.java
@@ -0,0 +1,13 @@
+package mk.finki.ukim.mk.earthquakeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EarthquakeAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EarthquakeAppApplication.class, args);
+    }
+
+}
---
+++ EarthquakeApp/src/test/java/mk/finki/ukim/mk/earthquakeapp/EarthquakeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package mk.finki.ukim.mk.earthquakeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EarthquakeAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
